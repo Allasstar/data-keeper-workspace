571f95d6ce0856f8bbb2499c5d56496ec4a56171
Roman Shulevka <micrarriors@gmail.com>
2025-03-16 20:04:22 +0200

    Test

--- Examples/Static/DebugSettings.cs
+++ Examples/Static/DebugSettings.cs
@@ -4,12 +4,14 @@ using DataKeeper.Attributes;
 [StaticClassInspector("Debug")]
 public static class DebugSettings
 {
+    private static bool EnableLogsPrivate = true;
     public static bool EnableLogs = true;
     public static bool DrawGizmos = true;
     public static LogLevel CurrentLogLevel = LogLevel.Warning;
     
-    public static List<string> TestStringList = new List<string>();
-    public static List<int> TestintList = new List<int>();
+    public static List<string> TestStringList = new List<string>() {"2", "4", "test"};
+    public static List<int> TestIntList = new List<int>() {1, 3, 5, 6};
+    public static bool[] TestBoolArray = new bool[4] { false, false, true, false };
     
     public enum LogLevel
     {
--- Examples/Static/GameSettings.cs
+++ Examples/Static/GameSettings.cs
@@ -6,6 +6,8 @@ using Vector3 = System.Numerics.Vector3;
 [StaticClassInspector()]
 public static class GameSettings
 {
+    private static float MasterVolumePrivate = 0.15f;
+
     public static float MasterVolume = 0.75f;
     public static bool MusicEnabled = true;
     public static int DifficultyLevel = 2;
@@ -14,6 +16,10 @@ public static class GameSettings
     public static Vector3 SpawnPoint = new Vector3(0, 1, 0);
 
     public static int PropertyMaxPlayers { get; set; } = 10;
+    
+    private static int PrivateProp{ get; set; } = 1;
+    public static int PrivateSetProp { get; private set; } = 3;
+
 
     public static readonly int MaxPlayers = 4; // This will be shown but not editable
     
