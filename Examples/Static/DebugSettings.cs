ac02bd1baa4ae041c8ab63dae980aaf17096ff24
Roman Shulevka <micrarriors@gmail.com>
2025-03-16 19:41:15 +0200

    Update DebugSettings.cs

--- Examples/Static/DebugSettings.cs
+++ Examples/Static/DebugSettings.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DataKeeper.Attributes;
 
 [StaticClassInspector("Debug")]
@@ -6,6 +7,9 @@ public static class DebugSettings
     public static bool EnableLogs = true;
     public static bool DrawGizmos = true;
     public static LogLevel CurrentLogLevel = LogLevel.Warning;
+    
+    public static List<string> TestStringList = new List<string>();
+    public static List<int> TestintList = new List<int>();
     
     public enum LogLevel
     {
